3d8bc98324936345c398269830711ec4d8c22052
Anila102 <59802564+Anila102@users.noreply.github.com>
2021-01-04 01:22:54 +0500

    Create Sum.c
    A simple c program to print addition of two inputted number

---
+++ Sum.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter 1st\n");
+    scanf("%d", &num1);
+    printf("Enter 2nd\n");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+    printf("sum is= %d", sum);
+
+    return 0;
+    num1
